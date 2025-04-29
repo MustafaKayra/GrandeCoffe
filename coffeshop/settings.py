986ae94c836a099edfb1b3741254fc56d9a21487
MustafaKayra <m.toker2009@gmail.com>
2025-04-30 00:34:44 +0300

    görseller üzerindeki ikonlar kaldırıldı

--- coffeshop/settings.py
+++ coffeshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '75d8477820831c010a4c35a20ba75777'
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
